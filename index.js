624777ad0bab6f789de22c9bffddc4fafb6d3682
Bram van der Veen <96aa48@gmail.com>
2015-08-12 14:42:49 +0200

    Intergrated the blns.json into the module

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
-var URL = 'https://raw.githubusercontent.com/minimaxir/big-list-of-naughty-strings/master/blns.json'
-var got = require('got');
+
 module.exports = function (cb) {
 	if (typeof cb !== 'function') throw new Error("Missing callback");
-	got(URL,cb)
+  cb(null, require('./resources/blns.json'));
 };
--- test.js
+++ test.js
@@ -2,9 +2,6 @@
 var assert = require('assert');
 var blns = require('./');
 
-it('should get a JSON object', function (done) {
-	blns(function(err,data){
-		assert.equal(Object.keys(JSON.parse(data)).length > 0, true);
-		done();
-	})
-});
+blns(function(err,data){
+  assert.equal(Object.keys(data).length > 0, true);
+})
